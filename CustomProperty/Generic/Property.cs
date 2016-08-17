321f7965948f5b92f1b6dc8a185ad5be04273c14
Nik Tomas <widauka@ya.ru>
2016-08-17 12:57:38 +0500

    Initial

---
+++ CustomProperty/Generic/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomProperty.Generic
+{
+    public class Property<T>
+    {
+        public delegate T Accessor();
+        public delegate void Mutator(T value);
+
+        public Accessor Get { get; set; }
+        public Mutator Set { get; set; }
+
+        public Property(Accessor get, Mutator set)
+        {
+            Get = get;
+            Set = set;
+        }
+    }
+}
+
---
+++ CustomProperty/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomProperty
+{
+    public class Property
+    {
+        public delegate object Accessor();
+        public delegate void Mutator(object value);
+
+        public Accessor Get { get; set; }
+        public Mutator Set { get; set; }
+
+        public Property(Accessor get, Mutator set)
+        {
+            Get = get;
+            Set = set;
+        }
+    }
+}
+
